b022468b7a8cf26f5995124eccd03d9a34ba05a5
ZRCW <2277917367@qq.com>
2025-07-04 11:29:26 +0800

    Update 主程序

--- 单纯形法/主程序.py
+++ 单纯形法/主程序.py
@@ -3,18 +3,4 @@ from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import Qt
 import sys
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("单元形法")
-        self.setGeometry(100, 100, 800, 600)
-        
-        # 设置窗口图标
-        self.setWindowIcon(QPixmap("icon.png"))
-        
-        # 设置窗口背景颜色
-        self.setStyleSheet("background-color: lightblue;")
-        
-        # 设置窗口标题栏文本颜色
-        self.setStyleSheet("QMainWindow { color: darkblue; }")
 
